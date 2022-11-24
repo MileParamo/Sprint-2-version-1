81a42196ce224da629b046771da28c8fce7644a5
Wilberto39 <113399895+Wilberto39@users.noreply.github.com>
2022-11-23 20:26:41 -0500

    Add files via upload

---
+++ Brigada-empresarial verson 2/src/main/java/com/marcaci/nbrigadistas/Brigada/empresarial/repository/BrigadistaRepository.java
@@ -0,0 +1,16 @@
+package com.marcaci.nbrigadistas.Brigada.empresarial.repository;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+import com.marcaci.nbrigadistas.Brigada.empresarial.model.Brigadistas;
+
+@Repository
+public interface BrigadistaRepository extends MongoRepository<Brigadistas, Long> {
+
+	@Query("{descripcion.'?0'}")
+	List<Brigadistas> findByDescripccion(String descripccion);
+}
+
---
+++ Brigada-empresarial verson 2/src/main/java/com/marcaci/nbrigadistas/Brigada/empresarial/service/BrigadistasServiceImpl.java
@@ -0,0 +1,66 @@
+package com.marcaci.nbrigadistas.Brigada.empresarial.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marcaci.nbrigadistas.Brigada.empresarial.model.Brigadistas;
+import com.marcaci.nbrigadistas.Brigada.empresarial.repository.BrigadistaRepository;
+
+@Service
+public class BrigadistasServiceImpl implements BrigadistasService {
+
+	@Autowired
+	private BrigadistaRepository repositorio;
+
+	@Override
+	public Brigadistas actualizarBrigadistas(Brigadistas brigadista) {
+		if (brigadista == null) {
+			return null;
+		}
+		Optional<Brigadistas> brigadistasAAtualizar = repositorio.findById(brigadista.getId());
+		if (brigadistasAAtualizar.isPresent()) {
+			return repositorio.save(brigadista);
+		}
+		return null;
+	}
+
+	@Override
+	public Brigadistas crearBrigadistas(Brigadistas brigadistas) {
+		Optional<Brigadistas> creaBrigadistas = repositorio.findById(brigadistas.getId());
+		if (creaBrigadistas.isPresent()) {
+		}
+		return repositorio.insert(brigadistas);
+
+	}
+
+	@Override
+	public void eliminarBrigadistasPorID(Long id) {
+		Optional<Brigadistas> eliminarBrigadista = repositorio.findById(id);
+		if (eliminarBrigadista.isPresent()) {
+			repositorio.delete(eliminarBrigadista.get());
+		}
+
+	}
+
+	@Override
+	public Optional<Brigadistas> consultarBrigadistas(Long id, String descripcion) {
+		if (id == null) {
+			return Optional.empty();
+		}
+		return repositorio.findById(id);
+	}
+
+	@Override
+	public List<Brigadistas> consultarBrigadistasPorId(Long Id) {
+		return null;
+	}
+
+	@Override
+	public List<Brigadistas> consultarLista() {
+		return repositorio.findAll();
+	}
+
+}
